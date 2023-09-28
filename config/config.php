a60d849302dcceb9aba7bf3254ae1b9b93170107
Last Lantican <lastdavid.lantican@gmail.com>
2023-09-28 10:04:44 +0800

    add config.php, required in index.php

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+//site name
+define('SITE_NAME', 'A Plain PHP CMS Template');
+
+//App Root
+define('APP_ROOT', dirname(dirname(__FILE__)));
+define('URL_ROOT', '/');
+define('URL_SUBFOLDER', '');
+
+//DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'plain-php-cms-template');
+define('DB_PASS', 'password');
+define('DB_NAME', 'plain-php-cms-template');
