f08e09cddabdef372bbb9dd173c14e2a9f3f11c0
Last Lantican <lastdavid.lantican@gmail.com>
2023-10-07 19:04:28 +0800

    add css

--- app/views/layouts/main_layout.php
+++ app/views/layouts/main_layout.php
@@ -5,6 +5,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo $page_title; ?></title>
+  <link rel="stylesheet" href="<?php echo ROOT_URL ?>/public/assets/stylesheets/application.css">
+  <link rel="stylesheet" href="<?php echo ROOT_URL ?>/public/assets/stylesheets/header.css">
+  <link rel="stylesheet" href="<?php echo ROOT_URL ?>/public/assets/stylesheets/footer.css">
 </head>
 
 <body>
